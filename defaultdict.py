0c579b7df1fabcfe4fba41109db1f609781d085d
Qianying Nie <qianying.nie@gmail.com>
2019-12-02 17:09:21 +0800

    Create defaultdict.py

---
+++ defaultdict.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+dict1 = defaultdict(int)
+dict2 = defaultdict(set)
+dict3 = defaultdict(str)
+dict4 = defaultdict(list)
+
+dict1[2] ='two'
+
+print(dict1[1])
+print(dict2[1])
+print(dict3[1])
+print(dict4[1])
+
+print(dict1[2])
+print(dict2[2])
+print(dict3[2])
+print(dict4[2])
