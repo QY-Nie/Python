490f51361674d6211e38e6f959cb42a6daa2363f
Qianying Nie <qianying.nie@gmail.com>
2019-12-02 16:12:22 +0800

    Create print_strings.py

---
+++ print_strings.py
@@ -0,0 +1,12 @@
+pint("Nie: Printing Strings in different way!")
+w1 = 'Hello '
+w2 = 'World!'
+words1 = 'Hello World!'
+words2 = w1 + w2
+print(f"print1: {words1}")
+print("print2: Hello World!")
+print("print3: " + words1)
+print("print4: ""Hello"" World!")
+print("print5: " + words2)
+print(f"print6: {w1}{w2}")
+print("print7: " + w1 + w2)
